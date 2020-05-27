f6dab4380b9e1b25c84c4d74e11261cf84dbd860
BuildTools <unconfigured@null.spigotmc.org>
2020-05-27 15:36:11 -0500

    Scraper and bot work together. Link IDs fetched.

--- GamesBot.py
+++ GamesBot.py
@@ -4,11 +4,12 @@ import os
 import discord
 from discord.ext import commands
 import json
+import RedditGamesScraper
 
 with open('config.json') as config_file:
     data = json.load(config_file)
 
-TOKEN = data['discord-token']
+TOKEN = data['discord'][0]['token']
 
 client = commands.Bot(command_prefix='./')
 
@@ -16,11 +17,18 @@ client = commands.Bot(command_prefix='./')
 @client.event
 async def on_ready():
     print('Connected!')
+    getNews()
 
 
 @client.event
 async def on_member_join(member):
     print(f'Bring back {member}... whats the point?')
+
+
+def getNews():
+    games = RedditGamesScraper.RedditGamesScraper()
+    links = games.getLinks()
+    print(links)
 
 
 client.run(TOKEN)
--- RedditGamesScraper.py
+++ RedditGamesScraper.py
@@ -1,21 +1,31 @@
 #! python3
 import praw
+import json
 
 MINIMUM_UPVOTE_RATIO = 0.78
 MINIMUM_UPVOTES = 850
 
-PUscript = 'Pq_F46VlaaQz2A'
-secret_key = 'ZFEhKhcl-_AVkY0907yCaz22sSw'
 
-reddit = praw.Reddit(client_id=PUscript,
-                     client_secret=secret_key,
-                     user_agent='GamesBot')
+class RedditGamesScraper():
 
-games = reddit.subreddit("games")
+    def __init__(self):
 
+        with open('config.json') as config_file:
+            data = json.load(config_file)
 
-def print_links():
-    for submission in games.hot(limit=20):
-        if (not submission.stickied and (not ("AMA" in submission.title))):
-            if(submission.upvote_ratio > MINIMUM_UPVOTE_RATIO and submission.score > MINIMUM_UPVOTES):
-                print(submission.title)
+        self.PUscript = data['reddit'][0]['PUscript']
+        self.secret_key = data['reddit'][0]['secret_key']
+
+        self.reddit = praw.Reddit(client_id=self.PUscript,
+                                  client_secret=self.secret_key,
+                                  user_agent='GamesBot')
+
+    def getLinks(self):
+        games = self.reddit.subreddit("games")
+        submissions = []
+
+        for submission in games.hot(limit=20):
+            if (not submission.stickied and (not ("AMA" in submission.title))):
+                if(submission.upvote_ratio > MINIMUM_UPVOTE_RATIO and submission.score > MINIMUM_UPVOTES):
+                    submissions.append(submission)
+        return submissions
