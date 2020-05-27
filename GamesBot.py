611daf9203de9be96e743229bc5881a6c9a15d44
BuildTools <unconfigured@null.spigotmc.org>
2020-05-27 14:46:19 -0500

    Discord bot initialized

--- GamesBot.py
+++ GamesBot.py
@@ -2,6 +2,25 @@
 
 import os
 import discord
+from discord.ext import commands
+import json
+
+with open('config.json') as config_file:
+    data = json.load(config_file)
+
+TOKEN = data['discord-token']
+
+client = commands.Bot(command_prefix='./')
 
 
-TOKEN =
+@client.event
+async def on_ready():
+    print('Connected!')
+
+
+@client.event
+async def on_member_join(member):
+    print(f'Bring back {member}... whats the point?')
+
+
+client.run(TOKEN)
