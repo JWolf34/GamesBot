1ee5d8d1d4828ee5004cd7b7517c78aa6bbd11d6
BuildTools <unconfigured@null.spigotmc.org>
2020-11-13 19:43:25 -0600

    Version 1.01

--- setup.py
+++ setup.py
@@ -2,12 +2,11 @@ from distutils.core import setup
 
 setup(
     name='GamesBot',
-    version = '1.0',
+    version = '1.01',
     description = 'A Discord bot for gaming news',
     author = 'John Wolf',
     author_email = 'johnmwolf34@gmail.com',
     url = 'https://github.com/JWolf34/GamesBot',
     packages = ['discord', 'asyncio'],
     py_modules=['GamesBot', 'RedditGamesScraper']
-
 )
