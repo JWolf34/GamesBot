9d68171c65a597be98327f7c568c5ba92688d0c7
BuildTools <unconfigured@null.spigotmc.org>
2020-11-13 18:43:55 -0600

    Rename changes

--- setup.py
+++ setup.py
@@ -2,6 +2,12 @@ from setuptools import setup
 
 setup(
     name='GamesBot',
+    version = '1.0',
+    description = 'A Discord bot for gaming news',
+    author = 'John Wolf',
+    author_email = 'johnmwolf34@gmail.com',
+    url = 'https://github.com/JWolf34/GamesBot',
+    packages = ['discord', 'asyncio'],
     py_modules=['GamesBot', 'RedditGamesScraper']
 
 )
